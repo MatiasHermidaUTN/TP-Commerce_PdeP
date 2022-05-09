0d2ff7befd0c8373a20dd8585fbad21a2ac03391
MatiasHermidaUTN <mhermida@frba.utn.edu.ar>
2022-05-08 22:47:27 -0300

    Estandarizar forma de definir parametros para los tipo Producto y usar fst y snd. Cambio de sintaxis en algunas funciones para no escribir el parámetro cuando no es necesario

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,8 +11,8 @@ type Precio = Float
 type Producto = (Nombre, Precio)
 
 precioTotal :: Producto -> Float -> Float -> Float -> Float
-precioTotal (_, precioDelProducto) cantidad descuento costoEnvio =
- (aplicarCostoDeEnvio costoEnvio).(* cantidad).(aplicarDescuento descuento) $ precioDelProducto
+precioTotal producto cantidad descuento costoEnvio =
+ (aplicarCostoDeEnvio costoEnvio).(* cantidad).(aplicarDescuento descuento).snd $ producto
 
 aplicarCostoDeEnvio :: Float -> Float -> Float
 aplicarCostoDeEnvio unCostoDeEnvio unPrecio = unPrecio + unCostoDeEnvio
@@ -21,28 +21,28 @@ aplicarDescuento :: Float -> Float -> Float
 aplicarDescuento unDescuento unPrecio = (*unPrecio).(1-) $ unDescuento --0<=descuento<=1
 
 entregaSencilla :: String -> Bool
-entregaSencilla unaFecha = (even.length) unaFecha
+entregaSencilla = (even.length)
 
 productoDeElite :: Producto -> Bool
 productoDeElite producto = ((&&).productoDeLujo $ producto).((&&).productoCodiciado $ producto).not.productoCorriente $ producto
 
 productoDeLujo :: Producto -> Bool
-productoDeLujo (nombreProducto, _) = elem 'x' nombreProducto || elem 'z'  nombreProducto || elem 'X' nombreProducto || elem 'Z'  nombreProducto
+productoDeLujo producto = ((elem 'x').(fst)) producto || ((elem 'z').(fst)) producto || ((elem 'X').(fst)) producto || ((elem 'Z').(fst)) producto
 
 productoCodiciado :: Producto -> Bool
-productoCodiciado (nombreProducto, _) = (>10).length $ nombreProducto
+productoCodiciado = (>10).length.fst
 
 productoCorriente :: Producto -> Bool
-productoCorriente (nombreProducto, _) = letraVocal.head $ nombreProducto
+productoCorriente = letraVocal.head.fst
 
 letraVocal :: Char -> Bool
 letraVocal unaLetra = elem unaLetra "aeiouAEIOU"
 
 productoXL :: Producto -> Producto
-productoXL (nombreProducto, precioDelProducto) = (nombreProducto ++ " XL", precioDelProducto)
+productoXL producto = ((++ "XL").fst $ producto, snd producto)
 
 versionBarata :: Producto -> Producto
 versionBarata producto = (reverse.fst.descodiciarProducto $ producto, snd producto)
 
 descodiciarProducto :: Producto -> Producto
-descodiciarProducto (nombreProducto, precioDelProducto) = (take 10 nombreProducto, precioDelProducto)
+descodiciarProducto producto = ((take 10).fst $ producto, snd producto)
