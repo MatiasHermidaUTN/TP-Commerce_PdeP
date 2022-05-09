c5d313340c8f8647a1b4ccab05a03268008b35c0
MatiasHermidaUTN <mhermida@frba.utn.edu.ar>
2022-05-08 21:55:07 -0300

    Eliminada la 'T' inicial y acortadas las definiciones de tipos cuando se usa type. Separados los elementos de las tuplas con espacio

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,12 +6,12 @@ someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 ---------------------------------------------------------------------------------------------------------------------------------------------------
 
-type TNombreDelProducto = String
-type TPrecioDelProducto = Float
-type TProducto = (TNombreDelProducto,TPrecioDelProducto)
+type NombreDelProducto = String
+type PrecioDelProducto = Float
+type Producto = (NombreDelProducto, PrecioDelProducto)
 
-precioTotal :: TProducto -> Float -> Float -> Float -> Float
-precioTotal (_,precioDelProducto) cantidad descuento costoEnvio =
+precioTotal :: Producto -> Float -> Float -> Float -> Float
+precioTotal (_, precioDelProducto) cantidad descuento costoEnvio =
  aplicarCostoDeEnvio (aplicarDescuento precioDelProducto descuento * cantidad) costoEnvio
 
 aplicarCostoDeEnvio :: Float -> Float -> Float
@@ -23,26 +23,26 @@ aplicarDescuento unPrecio unDescuento = unPrecio * (1-unDescuento) --0<=descuent
 entregaSencilla :: String -> Bool
 entregaSencilla unaFecha = (even.length) unaFecha
 
-productoDeElite :: TProducto -> Bool
+productoDeElite :: Producto -> Bool
 productoDeElite producto = productoDeLujo producto && productoCodiciado producto && (not.productoCorriente)  producto
 
-productoDeLujo :: TProducto -> Bool
-productoDeLujo (nombreProducto,_) = elem 'x' nombreProducto || elem 'z'  nombreProducto || elem 'X' nombreProducto || elem 'Z'  nombreProducto
+productoDeLujo :: Producto -> Bool
+productoDeLujo (nombreProducto, _) = elem 'x' nombreProducto || elem 'z'  nombreProducto || elem 'X' nombreProducto || elem 'Z'  nombreProducto
 
-productoCodiciado :: TProducto -> Bool
-productoCodiciado (nombreProducto,_) = length nombreProducto > 10
+productoCodiciado :: Producto -> Bool
+productoCodiciado (nombreProducto, _) = length nombreProducto > 10
 
-productoCorriente :: TProducto -> Bool
-productoCorriente (nombreProducto,_) = letraVocal.head $ nombreProducto
+productoCorriente :: Producto -> Bool
+productoCorriente (nombreProducto, _) = letraVocal.head $ nombreProducto
 
 letraVocal :: Char -> Bool
 letraVocal unaLetra = elem unaLetra "aeiouAEIOU"
 
-productoXL :: TProducto -> TProducto
-productoXL (nombreProducto,precioDelProducto) = (nombreProducto ++ " XL" , precioDelProducto)
+productoXL :: Producto -> Producto
+productoXL (nombreProducto, precioDelProducto) = (nombreProducto ++ " XL", precioDelProducto)
 
-versionBarata :: TProducto -> TProducto
-versionBarata producto = (reverse.fst.descodiciarProducto $ producto , snd producto)
+versionBarata :: Producto -> Producto
+versionBarata producto = (reverse.fst.descodiciarProducto $ producto, snd producto)
 
-descodiciarProducto :: TProducto -> TProducto
-descodiciarProducto (nombreProducto,precioDelProducto) = (take 10 nombreProducto,precioDelProducto)
+descodiciarProducto :: Producto -> Producto
+descodiciarProducto (nombreProducto, precioDelProducto) = (take 10 nombreProducto, precioDelProducto)
